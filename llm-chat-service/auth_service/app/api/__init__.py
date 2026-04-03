027e6cb4cb3de8418a6fe60ea0b357839b3bfa74
Iuliia <prusova.work@gmail.com>
2026-04-03 22:03:53 +0300

    добавлен роутер health

--- llm-chat-service/auth_service/app/api/__init__.py
+++ llm-chat-service/auth_service/app/api/__init__.py
@@ -1,9 +1,11 @@
 from .router_auth import router as auth_router
+from .router_health import health_router
 
 
 routers = [auth_router]
 
 
 __all__ = [
-    "routers"
+    "auth_router",
+    "health_router",
 ]
---
+++ llm-chat-service/auth_service/app/api/router_health.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+from auth_service.app.core.config import settings
+from auth_service.app.schemas.health_check import HealthCheck
+
+health_router = APIRouter(prefix="/health", tags=["health"])
+
+
+@health_router.get(
+    "/",
+    summary="Health check",
+    description="Проверка работоспособности сервера",
+    response_model=HealthCheck
+)
+async def health_check():
+    """
+    Проверка работоспособности сервера.
+    """
+    return HealthCheck(
+        status="OK",
+        environment=settings.env
+    )
