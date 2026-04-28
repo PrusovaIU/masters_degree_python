c02e94a420b179bc7c48a9aa7d06c784daedfd2b
Iuliia <prusova.work@gmail.com>
2026-04-28 17:27:16 +0300

    добавлен базовый http клиент

--- llm-chat-service/web_service/app/core/utils/httpx_client.py
+++ llm-chat-service/web_service/app/core/utils/httpx_client.py
@@ -1,12 +1,12 @@
+from collections.abc import AsyncGenerator
 from functools import wraps
 from typing import Type
-from httpx import AsyncClient, HTTPStatusError, Timeout, TimeoutException, \
-    Response
+from httpx import AsyncClient, HTTPStatusError, Timeout, TimeoutException
 from loguru import logger
-from web_service.app.core.exceptions.auth_client import AuthClientError
+from web_service.app.core.exceptions.base import AppException
 
 
-def error_handler_decorator(err_type: Type[AuthClientError], title: str):
+def error_handler_decorator(err_type: Type[AppException], title: str):
     """
     Декоратор для обработки ошибок, возникающих в httpx клиенте.
 
@@ -21,9 +21,50 @@ def error_handler_decorator(err_type: Type[AuthClientError], title: str):
                 return await func(*args, **kwargs)
             except HTTPStatusError as err:
                 logger.error(
-                    f"{title}: auth_service вернул ошибку "
-                    f"{err.response.status_code} ({err.response.text})"
+                    f"{title}: status error "
+                    f"({err.response.status_code} - {err.response.text})"
                 )
                 raise err_type(
-                    f"{err.response.status_code} ({err.response.text})}"
+                    f"{err.response.status_code} ({err.response.text})"
                 )
+            except TimeoutException:
+                logger.error(f"{title}: timeout error")
+                raise err_type("timeout error")
+            except Exception as err:
+                logger.error(f"{title}: {err} ({err.__class__.__name__})")
+                raise err_type(title)
+        return wrapper
+    return decorator
+
+
+class BaseClient:
+    """
+    Базовый клиент для работы с HTTP API.
+
+    :param url: URL сервиса.
+    :param timeout: Таймаут запроса.
+    :param auth_token_name: Имя заголовка с токеном авторизации.
+    """
+    def __init__(self, url: str, timeout: float, auth_token_name: str):
+        self._base_url = url
+        self._timeout = Timeout(timeout)
+        self._auth_token_name = auth_token_name
+
+    async def _get_client(
+            self,
+            access_token: str  | None = None
+    ) -> AsyncGenerator[AsyncClient, None]:
+        """
+        Получение HTTP-клиента.
+
+        :param access_token: Access токен. Если None, то запрос не авторизован.
+        :return: HTTP-клиент.
+        """
+        async with AsyncClient(
+                base_url=self._base_url,
+                timeout=self._timeout
+        ) as client:
+            if access_token:
+                header = f"Bearer {access_token}"
+                client.headers[self._auth_token_name] = header
+            yield client
--- llm-chat-service/web_service/app/services/auth_client.py
+++ llm-chat-service/web_service/app/services/auth_client.py
@@ -2,9 +2,7 @@ from collections.abc import AsyncGenerator
 
 from httpx import AsyncClient, HTTPStatusError, Timeout, TimeoutException, \
     Response
-from typing import Optional
 
-from web_service.app.core.config import settings
 from libs.schemas.auth import (
     RegisterRequest, RegisterResponse, LoginResponse,
     RefreshTokenRequest, RefreshTokenResponse
@@ -12,42 +10,14 @@ from libs.schemas.auth import (
 from libs.schemas.user import UserPublic
 from web_service.app.core.exceptions import auth_client as errs
 from loguru import logger
+from web_service.app.core.utils.httpx_client import error_handler_decorator, BaseClient
 
 
 
-class AuthClient:
+class AuthClient(BaseClient):
     """
-    HTTP-клиент для взаимодействия с auth_service.
-
-    :param url: URL auth_service.
-    :param timeout: Таймаут запроса.
-    :param auth_token_name: Имя заголовка с токеном авторизации.
+    Клиент для взаимодействия с auth_service.
     """
-
-    def __init__(self, url: str, timeout: float, auth_token_name: str):
-        self._base_url = url
-        self._timeout = Timeout(timeout)
-        self._auth_token_name = auth_token_name
-
-    async def _get_client(
-            self,
-            access_token: str  | None = None
-    ) -> AsyncGenerator[AsyncClient, None]:
-        """
-        Получение HTTP-клиента.
-
-        :param access_token: Access токен. Если None, то запрос не авторизован.
-        :return: HTTP-клиент.
-        """
-        async with AsyncClient(
-                base_url=self._base_url,
-                timeout=self._timeout
-        ) as client:
-            if access_token:
-                header = f"Bearer {access_token}"
-                client.headers[self._auth_token_name] = header
-            yield client
-
     async def register(
             self,
             data: RegisterRequest
@@ -136,6 +106,10 @@ class AuthClient:
                 str(username)
             )
 
+    @error_handler_decorator(
+        errs.RefreshTokenError,
+        "Ошибка обновления токена"
+    )
     async def refresh_token(
             self,
             refresh_token: str
@@ -148,34 +122,21 @@ class AuthClient:
 
         :raises RefreshTokenError: Ошибка обновления токена.
         """
-        try:
-            data = RefreshTokenRequest(
-                refresh_token=refresh_token
-            ).model_dump()
-            async with self._get_client() as client:
-                resp = await client.post(
-                    "/auth/refresh",
-                    json=data
-                )
-                resp.raise_for_status()
-            return RefreshTokenResponse(**resp.json())
-        except HTTPStatusError:
-            logger.error(
-                f"Ошибка обновления токена: {resp.text} "
-                f"(status={resp.status_code})"
+        data = RefreshTokenRequest(
+            refresh_token=refresh_token
+        ).model_dump()
+        async with self._get_client() as client:
+            resp = await client.post(
+                "/auth/refresh",
+                json=data
             )
-            raise errs.RefreshTokenError(
-                f"{resp.text} (status={resp.status_code})"
-            )
-        except TimeoutException:
-            logger.error(f"Ошибка обновления токена: timeout error")
-            raise errs.RefreshTokenError("timeout error")
-        except Exception as err:
-            logger.error(
-                f"Ошибка обновления токена: {err} ({err.__class__.__name__})"
-            )
-            raise errs.RefreshTokenError("Ошибка обновления токена")
+            resp.raise_for_status()
+        return RefreshTokenResponse(**resp.json())
 
+    @error_handler_decorator(
+        errs.ProfileError,
+        "Ошибка получения профиля"
+    )
     async def get_me(self, access_token: str) -> UserPublic | None:
         """
         Получение профиля текущего пользователя.
@@ -186,20 +147,7 @@ class AuthClient:
 
         :raises ProfileError: Ошибка получения профиля.
         """
-        title_err = "Ошибка получения профиля пользователя"
-        try:
-            async with self._get_client(access_token) as client:
-                resp = await client.get("/auth/me")
-                resp.raise_for_status()
-                return UserPublic(**resp.json())
-        except HTTPStatusError:
-            logger.error(
-                f"{title_err}: {resp.text} (status={resp.status_code})"
-            )
-            raise errs.ProfileError(f"{resp.text} (status={resp.status_code})")
-        except TimeoutException:
-            logger.error(f"{title_err}: timeout error")
-            raise errs.ProfileError(f"timeout error")
-        except Exception as err:
-            logger.error(f"{title_err}: {err} ({err.__class__.__name__})")
-            raise errs.ProfileError(title_err)
+        async with self._get_client(access_token) as client:
+            resp = await client.get("/auth/me")
+            resp.raise_for_status()
+            return UserPublic(**resp.json())
